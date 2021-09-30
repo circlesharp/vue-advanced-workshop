5f2715764a95dd100ec4b71876f7c88172b1b8d9
circlesharp <circlesharp@live.com>
2021-10-01 02:26:06 +0800

    feat(1.2): 通过观测订阅模式实现响应式

--- 1-reactivity/__test__/1.2.test.js
+++ 1-reactivity/__test__/1.2.test.js
@@ -1,18 +1,34 @@
 require('../../util').createTestCase(__filename, (window, logs, done) => {
-  const { Dep, autorun } = window
+  const { Dep, autorun } = window;
 
-  const dep = new Dep()
+  const dep = new Dep();
 
   autorun(() => {
-    dep.depend()
-    window.console.log('updated')
-  })
-  expect(logs.length).toBe(1)
-  expect(logs[0][0]).toMatch(`updated`)
+    dep.depend();
+    window.console.log('updated');
+  });
+  expect(logs.length).toBe(1);
+  expect(logs[0][0]).toMatch(`updated`);
 
-  dep.notify()
-  expect(logs.length).toBe(2)
-  expect(logs[1][0]).toMatch(`updated`)
+  dep.notify();
+  expect(logs.length).toBe(2);
+  expect(logs[1][0]).toMatch(`updated`);
 
-  done()
-})
+  // 我新增的测试用例
+  const FIRST_UPDATED = 'updated';
+  const SECOND_UPDATED = 'second_updated';
+  autorun(() => {
+    dep.depend();
+    window.console.log(SECOND_UPDATED);
+  });
+
+  expect(logs.length).toBe(3);
+  expect(logs[2][0]).toMatch(SECOND_UPDATED);
+
+  dep.notify();
+  expect(logs.length).toBe(5);
+  expect(logs[3][0]).toMatch(FIRST_UPDATED);
+  expect(logs[4][0]).toMatch(SECOND_UPDATED);
+
+  done();
+});
